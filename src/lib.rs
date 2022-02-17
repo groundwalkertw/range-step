7c6cd2dc77b333f5b22dd6f161b4af9fc697c69a
Jhih-Yuan Kao <frankintp93830@hotmail.com>
2022-02-17 16:15:18 +0800

    start

---
+++ src/lib.rs
@@ -0,0 +1,109 @@
+use std::ops::Range;
+
+macro_rules! impl_range {
+    ($($t: ty)*) => {
+        $(
+            impl From<Range<$t>> for ERange<$t>
+            {
+                fn from(range: Range<$t>) -> Self {
+                    if range.start < range.end {
+                        crate::ERange::<$t>::from_range(range, 1 as $t)
+                    } else {
+                        crate::ERange::<$t>::from_range(range, -1 as $t)
+                    }
+                }
+            }
+
+        )*
+    };
+}
+
+#[macro_export]
+macro_rules! from_range {
+    ($range: expr, $step: expr) => {
+        crate::ERange::from_range($range, $step)
+    };
+    ($range: expr) => {
+        crate::ERange::from($range)
+    }
+}
+
+impl_range!(i8 i16 i32 i64 i128 isize f32 f64);
+
+pub struct ERange<T> 
+    where T: std::ops::Add<Output = T> + PartialEq + PartialOrd + Clone + Copy
+{   
+    end: T,
+    step: T,
+    now: T,
+    direction: Direction
+}
+enum Direction {
+    Plus,
+    Minus,
+    Stop,
+}
+
+impl<T> ERange<T> 
+    where T: std::ops::Add<Output = T> + PartialEq + PartialOrd + Clone + Copy
+{   
+    #[inline]
+    fn new(start: T, end: T, step: T) -> Self {
+        let direction = if start <= end && start + step < end {
+            Direction::Plus
+        } else if start >= end && start + step > end {
+            Direction::Minus
+        } else {
+            Direction::Stop
+        };
+
+        ERange {
+            end,
+            step,
+            now: start,
+            direction,
+        }
+    }
+    #[inline]
+    fn from_range(range: Range<T>, step: T) -> Self {
+        Self::new(range.start, range.end, step)
+    }
+}
+
+impl<T> Iterator for ERange<T> 
+    where T: std::ops::Add<Output = T> + PartialEq + PartialOrd + Clone + Copy
+{
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {        
+        let return_now = self.now;
+        self.now = self.now + self.step;
+
+        match self.direction {
+            Direction::Plus => {                                
+                if self.now >= self.end {
+                    self.direction = Direction::Stop;
+                }
+                Some(return_now)
+            },
+            Direction::Minus => {
+                if self.now <= self.end {
+                    self.direction = Direction::Stop;
+                }
+                Some(return_now)
+            },
+            Direction::Stop => {
+                None
+            }
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result: Vec<i32> = from_range!(5..1).collect();
+        assert_eq!(vec![5, 4, 3, 2], result);
+    }
+}
